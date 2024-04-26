e36133fd5b279c90afaeeaa5556a7c1075b5a022
Khaleel106 <110684595+Khaleel106@users.noreply.github.com>
2024-04-26 19:09:13 +0530

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -8,6 +8,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { lgym } from "../constants/index.jsx";
 import vid from "../assets/video.mp4";
+import tpic from "../assets/pic.jpg";
 function loadingAnimation() {
   gsap.from("#page1 h1 ", {
     y: 100,
@@ -94,6 +95,42 @@ const Landing = () => {
         From weightlifting to cardio, we've got everything you need to elevate
         your workout routine."
       </h3>
+       <div
+        id="trainer"
+        className="w-full h-auto lg:h-screen flex flex-col lg:flex-row items-center justify-between bg-neutral-900 "
+      >
+        <img
+          data-scroll
+          data-scroll-speed="0.5"
+          className="w-full overflow-hidden lg:w-auto h-auto sm:h-80 lg:h-full rounded-md lg:rounded-none"
+          src={tpic}
+          alt="transformation"
+        />
+        <div className="my-11 mx-8 lg:w-1/2 lg:my-auto">
+          <div className="text-3xl lg:text-4xl xl:text-5xl">Hi, my name is</div>
+          <div className="text-5xl lg:text-7xl xl:text-8xl">
+            Muhammad Saifulla
+          </div>
+          <div className="text-4xl lg:text-6xl xl:text-6xl my-5">
+            And I'm a{" "}
+            <span className="bg-gradient-to-r from-orange-400 to-orange-600 text-transparent bg-clip-text">
+              Certified Nutrition Expert and Fitness Consultant
+            </span>
+          </div>
+          <div className="text-base lg:text-lg xl:text-2xl">
+            Transform your life just like I did. I know the struggle of facing
+            LOW CONFIDENCE, HEALTH RISKS due to being overweight firsthand. But
+            I made a choice to change. Through learning about nutrition and
+            hitting the gym regularly, I transformed my body and reclaimed my
+            health. If you're ready to start your own journey, join me at{" "}
+            <span className="bg-gradient-to-r from-orange-400 to-orange-600 text-transparent bg-clip-text">
+              ArnoldGym
+            </span>
+            . Let's make those fitness goals a reality together!
+          </div>
+        </div>
+      </div>
+      
       {/* //ladies section */}
       <div className="mt-5 my-10">
         <h2 className="text-2xl xl:text-5xl text-center">
@@ -116,11 +153,11 @@ const Landing = () => {
                       className="px-5 my-5 "
                     >
                       <img
-                       
-                        src={testimonial.pic}
-                        alt={`User ${index}`}
-                        className=" mx-auto w-screen h-fit mb-4"
-                      />
+                      src={testimonial.pic}
+                      alt={`User ${index}`}
+                      className="mx-auto w-full max-w-screen h-auto object-contain mb-4"
+                      style={{ maxHeight: '100%', maxWidth: '100%' }}
+                    />
                     </div>
                   ))}
                 </Slider>
